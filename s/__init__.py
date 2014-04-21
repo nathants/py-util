538a0cea5642733b78316156115361d71c4c63a7
nathants <me@nathants.com>
2014-04-21 18:56:03 -0500

    absolute import all things!

--- s/__init__.py
+++ s/__init__.py
@@ -1,2 +1,3 @@
+from __future__ import absolute_import
 from s.hacks import ModuleRedirector
 ModuleRedirector(__name__, lambda x: __import__('s.{}'.format(x), fromlist='*'))
--- s/data.py
+++ s/data.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import
 import s
 import types
 import functools
