38a8c9161dab36d789e6a6f468150c43e7ae1480
nathants <me@nathants.com>
2014-04-21 18:53:32 -0500

    changes to the way the stack is built and read

--- s/fn.py
+++ s/fn.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import pprint
 import sys
 import functools
 import traceback
@@ -12,8 +13,8 @@ import s
 _state = {}
 
 
-def _pretty(name, char, offset=0):
-    return '-' * (len(stack()) - offset) + char + '' + '|' + name
+def _pretty(name, char):
+    return '-' * (len(stack()) + 1) + char + '' + '|' + name
 
 
 trace_funcs = {
@@ -28,7 +29,7 @@ trace_funcs = {
              'stack': _format_stack(stack())}
         ]),
         'out': lambda name, val=None, traceback=None: logging.debug([
-            _pretty(name, '<', 1),
+            _pretty(name, '<'),
             {'value': val,
              'direction': 'out',
              'time': time.time(),
@@ -50,9 +51,9 @@ def _format_stack(val):
 
 
 @contextlib.contextmanager
-def state_layer(kind, func_name):
-    _bak = _state['_stack'] = _state.get('_stack', ())
-    _state['_stack'] += ([kind, func_name],)
+def state_layer(kind, name):
+    _bak = _state['_stack'] = _state.get('_stack') or ()
+    _state['_stack'] += ((kind, name),)
     try:
         yield
     except:
@@ -63,9 +64,9 @@ def state_layer(kind, func_name):
         _state['_stack'] = _bak
 
 
-def state():
+def state(offset=0):
     try:
-        return (_state.get('_stack') or ())[-1][0]
+        return (_state.get('_stack') or ())[-(1 + offset)][0]
     except IndexError:
         return ()
 
@@ -78,8 +79,8 @@ def _module_name(decoratee):
     module = decoratee.__module__
     with s.exceptions.ignore():
         if module == '__main__':
-            for x in range(100):
-                _module = '.'.join(__file__.split('.')[0].split('/')[-x:])
+            for x in range(20):
+                _module = '.'.join(__file__.split('.')[0].split('/')[x:])
                 if _module in sys.modules:
                     return _module
     return module
@@ -91,34 +92,47 @@ def fn_type(kind, rules, skip_return_check=False):
         def decorated(*a, **kw):
             name = '{}:{}()'.format(_module_name(decoratee), decoratee.__name__)
             trace_funcs[kind]['in'](name, *a, **kw)
-            rules()
             with state_layer(kind, name):
                 try:
+                    rules()
                     val = decoratee(*a, **kw)
                 except:
                     trace_funcs[kind]['out'](name, traceback=traceback.format_exc())
                     raise
                 else:
-                    trace_funcs[kind]['out'](name, val)
                     assert skip_return_check or val is not None, 'return data, not None, from function: {}'.format(name)
-                    return val
+            trace_funcs[kind]['out'](name, val)
+            return val
         return decorated
     return decorator
 
 
+def _rule_violation_message():
+    caller = s.hacks.get_caller(4)
+    with open(caller.filename) as fio:
+        line = fio.read().splitlines()[caller.linenum - 1].strip()
+    return '\n'.join([
+        '',
+        '[{}:{}] {}'.format(caller.filename, caller.linenum, line),
+        'attempted illegal transtion: {} -> {}'.format(*stack()[-2:]),
+        'stack:\n{}'.format(pprint.pformat(stack())),
+        '',
+    ])
+
+
 def glue_rules():
-    assert state() != 'logic', 'logic cannot contain glue: {}'.format(s.hacks.get_caller(3))
+    assert state(offset=1) != 'logic', 'logic cannot contain glue\n{}'.format(_rule_violation_message())
 glue = fn_type('glue', glue_rules)
 
 
 # todo, define better the difference between glue and flow. are two types necessary?
 def flow_rules():
-    assert state() != 'logic', 'logic cannot contain flow: {}'.format(s.hacks.get_caller(3))
+    assert state(offset=1) != 'logic', 'logic cannot contain flow\n{}'.format(_rule_violation_message())
 flow = fn_type('flow', flow_rules)
 
 
 def logic_rules():
-    assert state() != 'glue', 'glue cannot contain logic: {}'.format(s.hacks.get_caller(3))
+    assert state(offset=1) != 'glue', 'glue cannot contain logic\n{}'.format(_rule_violation_message())
 logic = fn_type('logic', logic_rules)
 
 
