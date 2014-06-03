d0f72dcb09452189853505cbd5a0c7ac52f9c43f
nathants <me@nathants.com>
2014-06-02 21:59:30 -0500

    update test for privatized functions

--- test_s/fast/fn.py
+++ test_s/fast/fn.py
@@ -3,18 +3,18 @@ import s
 
 
 def test_stack():
-    start = s.fn.stack()
+    start = s.fn._stack()
     @s.fn.logic
     def fn1():
-        assert s.fn.stack() == ('logic:{}:fn1'.format(__name__),)
+        assert s.fn._stack() == ('logic:{}:fn1'.format(__name__),)
         return fn2()
     @s.fn.logic
     def fn2():
-        assert s.fn.stack() == ('logic:{}:fn1'.format(__name__),
+        assert s.fn._stack() == ('logic:{}:fn1'.format(__name__),
                                 'logic:{}:fn2'.format(__name__))
         return True
     fn1()
-    assert s.fn.stack() == start
+    assert s.fn._stack() == start
 
 
 def test_flow_in_logic():
@@ -85,11 +85,11 @@ def test_logic_generator():
     @s.fn.logic
     def logic():
         for x in range(3):
-            assert s.fn.stack() == ('logic:test_s.fast.fn:logic',)
+            assert s.fn._stack() == ('logic:test_s.fast.fn:logic',)
             yield x
     for i, x in enumerate(logic()):
         assert i == x
-        assert s.fn.stack() == ()
+        assert s.fn._stack() == ()
 
 
 def test_logic_raise():
