b5310c21b9717dc96f50baa7c5caa16bbecde135
nathants <me@nathants.com>
2014-06-08 20:13:03 -0500

    version bump and other attributes

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import os
 import setuptools
 
+
 package_data = {'': ['/'.join(os.path.join(path, file).split('/')[1:])
                      for n in os.listdir('.')
                      if os.path.isdir(n)
@@ -10,10 +11,14 @@ package_data = {'': ['/'.join(os.path.join(path, file).split('/')[1:])
                      if not any(x.startswith('.') for x in path.split('/') + [file])
                      and not file.endswith('.pyc')]}
 
+
 kwargs = dict(
+    version="2",
     license='mit',
     name="shared",
-    version="1",
+    author='nathan todd-stone',
+    author_email='me@nathants.com',
+    url='http://github.com/nathants/shared',
     packages=setuptools.find_packages(),
     package_data=package_data,
     entry_points={'console_scripts': [
@@ -22,5 +27,6 @@ kwargs = dict(
         'derive-types = s.bin.derive_types:main',
     ]},
 )
+
 
 setuptools.setup(**kwargs)
