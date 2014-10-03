364df7ea22650cee467265bdca7104df8898b6fd
nathants <me@nathants.com>
2014-10-02 22:42:37 -0700

    remove cruft from setup.py

--- setup.py
+++ setup.py
@@ -2,40 +2,22 @@ import os
 import setuptools
 
 
-package_data = {'': ['/'.join(os.path.join(path, file).split('/')[1:])
-                     for n in os.listdir('.')
-                     if os.path.isdir(n)
-                     and '__init__.py' in os.listdir(n)
-                     for path, dirs, files in os.walk(n)
-                     for file in files
-                     if not any(x.startswith('.') for x in path.split('/') + [file])
-                     and not file.endswith('.pyc')]}
-
-
-kwargs = dict(
+setuptools.setup(
     version="0.0.2",
     license='mit',
-    name="shared",
+    name="s",
     author='nathan todd-stone',
     author_email='me@nathants.com',
-    url='http://github.com/nathants/shared',
+    url='http://github.com/nathants/s',
+    install_requires=open('requirements.txt').readlines(),
     packages=setuptools.find_packages(),
-    package_data=package_data,
     entry_points={'console_scripts': [
-        'debug = s.bin.debug:main',
-        'auto-test = s.bin.auto_test:main',
-        'derive-types = s.bin.derive_types:main',
+        '{} = s.bin.{}:main'.format(
+            x.replace('.py', '').replace('_', '-'),
+            x.replace('.py', '')
+        )
+        for x in os.listdir('s/bin')
+        if not x.startswith('_')
+        and x.endswith('.py')
     ]},
-    install_requires=[
-        'argh',
-        'blessed',
-        'pager',
-        'pytest',
-        'tornado',
-        'futures',
-        'requests',
-    ]
 )
-
-
-setuptools.setup(**kwargs)
