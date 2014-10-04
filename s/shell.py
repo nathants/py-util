39d0e4d8f0ee6e86302e5e19b1f4786d3d06307b
nathants <me@nathants.com>
2014-10-03 22:09:36 -0700

    use isinstance

--- s/shell.py
+++ s/shell.py
@@ -205,7 +205,7 @@ def walk_files_mtime(directories=['.'], predicate=lambda filepath: True):
 def dispatch_commands(_globals, _name_):
     argh.dispatch_commands(sorted([
         v for k, v in _globals.items()
-        if type(v) == types.FunctionType
+        if isinstance(v, types.FunctionType)
         and v.__module__ == _name_
         and not k.startswith('_')
     ], key=lambda x: x.__name__))
