49b388dc7d7acbf049e33cf5707d1cc5d8dd4706
nathants <me@nathants.com>
2014-10-04 21:22:24 -0700

    expand get prefs tests

--- test_s/fast/shell.py
+++ test_s/fast/shell.py
@@ -1,7 +1,6 @@
 import s
 import os
 import mock
-import six
 
 
 def test_module_name():
@@ -27,3 +26,7 @@ def test_get_or_prompt_pref():
             with mock.patch('six.moves.input', mock.Mock(return_value='bar')) as _raw_input:
                 assert s.shell.get_or_prompt_pref('foo', __file__) == 'bar'
                 assert _raw_input.call_count == 1
+                assert s.shell.get_or_prompt_pref('foo', __file__) == 'bar'
+                assert _raw_input.call_count == 1
+                with open(s.shell.files()[0]) as _file:
+                    assert _file.read().strip() == 'foo: bar'
