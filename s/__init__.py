fa1d0889a8575b412828a91d76c89b39732388fb
nathants <me@nathants.com>
2014-10-04 21:21:15 -0700

    boilerplate

--- s/__init__.py
+++ s/__init__.py
@@ -1,3 +1,3 @@
-from __future__ import absolute_import
+from __future__ import print_function, absolute_import
 from s.hacks import ModuleRedirector
 ModuleRedirector(__name__, lambda x: __import__('s.{}'.format(x), fromlist='*'))
