3dfe47715c948647e41fbe062a87e2b349c83b12
nathants <me@nathants.com>
2014-10-06 16:03:12 -0700

    lets not be too fancy

--- s/bin/tests/__init__.py
+++ s/bin/tests/__init__.py
@@ -1,15 +1,9 @@
 from __future__ import print_function, absolute_import
-from s.bin.tests import auto as _auto
-from s.bin.tests import cover as _cover
-import s
+import s.bin.tests.auto
+import s.bin.tests.cover
+import argh
 
-
-def auto():
-    _auto.main()
-
-
-def cover():
-    _cover.main()
 
 def main():
-    s.shell.dispatch_commands(globals(), __name__)
+    argh.dispatch_commands([auto.auto,
+                            cover.cover])
