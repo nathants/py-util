7841017b419efc7a42e36db90be1f6b9fa0a3430
nathants <me@nathants.com>
2014-10-06 16:16:51 -0700

    imports bugfix

--- s/bin/tests/__init__.py
+++ s/bin/tests/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import print_function, absolute_import
-import s.bin.tests.auto
-import s.bin.tests.cover
+from s.bin.tests import auto
+from s.bin.tests import cover
 import argh
 
 
