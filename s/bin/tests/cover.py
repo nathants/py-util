16c7ba6f24370482e582359f4239ed6f6dfec548
nathants <me@nathants.com>
2014-10-21 22:14:06 -0700

    coverage entrypoint

--- s/bin/tests/cover.py
+++ s/bin/tests/cover.py
@@ -1,5 +1,18 @@
 from __future__ import print_function, absolute_import
+import s
 
 
 def cover():
-    print('cover!')
+    for test_file in s.test.fast_test_files():
+        data = s.test._cover(test_file)
+        if data:
+            text = ''
+            text += s.colors.yellow(data['name']) + ' '
+            text += getattr(s.colors, 'green' if data['percent'] == '100' else 'red')(data['percent'] + '% ')
+            print(text)
+            if data['missing']:
+                missing_text = ''
+                missing_count = 5
+                missing_text += (', '.join(data['missing'][:missing_count]) +
+                                 ('...' if len(data['missing']) > missing_count else ''))
+                print(s.strings.indent(missing_text, 1))
