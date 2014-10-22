7b468d27cfffee39fc96d0fd76d81cbff4af6178
nathants <me@nathants.com>
2014-10-21 23:20:31 -0700

    new test for s.data

--- test_s/fast/data.py
+++ test_s/fast/data.py
@@ -2,30 +2,35 @@ import s
 import pytest
 
 
-def test_setitem():
+def test_setitem_immutalize():
     with pytest.raises(ValueError):
         s.data.immutalize({'a': 1})['a'] = 2
 
 
-def test_getitem():
+def test_append_immutalize():
+    with pytest.raises(AttributeError):
+        s.data.immutalize([]).append(1)
+
+
+def test_getitem_immutalize():
     assert 1 == s.data.immutalize({'a': 1})['a']
 
 
-def test_immutalize_set():
+def test_set_immutalize():
     x = {1, 2, 3}
     y = s.data.immutalize(x)
     x.add(4)
     assert y == {1, 2, 3}
 
 
-def test_immutalize_nested():
+def test_nested_immutalize():
     x = {1}
     y = s.data.immutalize({'val': x})
     x.add(2)
     assert len(y['val']) == 1
 
 
-def test_immutalize_list():
+def test_list_immutalize():
     x = [1, 2, 3]
     y = s.data.immutalize(x)
     x.append(4)
