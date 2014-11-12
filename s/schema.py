bd8f50d6956e6dc952e1bf9b02e7f6d96436cc70
nathants <me@nathants.com>
2014-11-11 22:32:18 -0800

    schemas!

---
+++ s/schema.py
@@ -0,0 +1,46 @@
+from __future__ import print_function, absolute_import
+import s
+import types
+
+
+def validate(schema, value):
+    if isinstance(schema, dict):
+        assert isinstance(value, dict), 'value {}, should be a dict for schema: {}'.format(value, schema)
+        for k, v in value.items():
+            value_mismatch = k not in schema
+            type_mismatch = type(k) not in [x for x in schema if isinstance(x, type)]
+            if value_mismatch and type_mismatch:
+                raise ValueError('key: {} ({}), does not match schema keys: {}'.format(k, type(k), schema.keys()))
+            else:
+                _validate(schema[type(k)] if value_mismatch else schema[k], v)
+    else:
+        _validate(schema, value)
+    return value
+
+
+def _validate(validator, value):
+    if isinstance(validator, (list, tuple)):
+        if not isinstance(value, (list, tuple)):
+            raise ValueError('{} ({}) is not a {}'.format(value, type(value), validator))
+        elif isinstance(validator, list):
+            if len(validator) != 1:
+                raise ValueError('list validators represent variable length iterables and must contain a single validator: {}'.format(validator)) # noqa
+            for v in value:
+                _validate(validator[0], v)
+        elif isinstance(validator, tuple):
+            if len(validator) != len(value):
+                raise ValueError('{} ({}) mismatched length of validator {}'.format(value, type(value), validator))
+            for _validator, _val in zip(validator, value):
+                _validate(_validator, _val)
+    elif isinstance(validator, dict):
+        if not isinstance(value, dict):
+            raise ValueError('{} ({}) does not match schema {}'.format(value, type(value), validator))
+        validate(validator, value)
+    elif isinstance(validator, type):
+        if type(value) is not validator:
+            raise ValueError('{} ({}) is not a {}'.format(value, type(value), validator))
+    elif isinstance(validator, types.FunctionType):
+        if not validator(value):
+            raise ValueError('{} ({}) failed validator {}'.format(value, type(value), s.func.source(validator)))
+    elif value != validator:
+        raise ValueError('{} ({}) != {}'.format(value, type(value), validator))
---
+++ test_s/fast/schema.py
@@ -0,0 +1,141 @@
+from __future__ import print_function, absolute_import
+import s
+import pytest
+
+
+def test_single_type_schema():
+    schema = int
+    assert s.schema.validate(schema, 1) == 1
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, '1')
+
+
+def test_single_iterable_length_n():
+    schema = [int]
+    assert s.schema.validate(schema, [1, 2]) == [1, 2]
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, [1, '2'])
+
+
+def test_single_iterable_fixed_length():
+    schema = (float, int)
+    assert s.schema.validate(schema, [1.1, 2]) == [1.1, 2]
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, [1.1, '2'])
+
+
+def test_nested_type_to_type_mismatch():
+    schema = {str: {float: int}}
+    assert s.schema.validate(schema, {'1': {1.1: 1}}) == {'1': {1.1: 1}}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'1': None})
+
+
+def test_nested_type_to_type():
+    schema = {str: {float: int}}
+    assert s.schema.validate(schema, {'1': {1.1: 1}}) == {'1': {1.1: 1}}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'1': {1.1: '1'}})
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'1': {1: 1}})
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: {1.1: 1}})
+
+
+def test_type_to_type():
+    schema = {str: int}
+    assert s.schema.validate(schema, {'1': 1}) == {'1': 1}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: 1})
+
+
+def test_value_to_type():
+    schema = {'foo': int}
+    assert s.schema.validate(schema, {'foo': 1}) == {'foo': 1}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'foo': 'bar'})
+
+
+def test_value_to_value():
+    schema = {'foo': 'bar'}
+    assert s.schema.validate(schema, {'foo': 'bar'}) == {'foo': 'bar'}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'foo': 1})
+
+
+def test_value_to_validator():
+    schema = {'foo': lambda x: isinstance(x, int) and x > 0}
+    assert s.schema.validate(schema, {'foo': 1}) == {'foo': 1}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'foo': 0})
+
+
+def test_nested_value_to_validator():
+    schema = {'foo': {'bar': lambda x: isinstance(x, int) and x > 0}}
+    assert s.schema.validate(schema, {'foo': {'bar': 1}}) == {'foo': {'bar': 1}}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'foo': {'bar': 0}})
+
+
+def test_iterable_length_n_bad_validator():
+    schema = {str: [str, str]}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'blah': ['blah', 'blah']})
+
+
+def test_iterable_length_n():
+    schema = {str: [str]}
+    assert s.schema.validate(schema, {'1': ['1', '2']}) == {'1': ['1', '2']}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: 1})
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: ['1', 2]})
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {'1': None})
+
+
+def test_iterable_fixed_length():
+    schema = {str: (str, str)}
+    assert s.schema.validate(schema, {'1': ['1', '2']}) == {'1': ['1', '2']}
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: ['1']})
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: ['1', '2', '3']})
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, {1: ['1', 2]})
+
+
+def test_nested_iterables():
+    schema = {str: [[str]]}
+    assert s.schema.validate(schema, {'1': [['1'], ['2']]}) == {'1': [['1'], ['2']]}
+    with pytest.raises(ValueError):
+        assert s.schema.validate(schema, {'1': [['1'], [1]]})
+
+
+def test_complex_types():
+    schema = {'name': str,
+              'age': lambda x: isinstance(x, int) and x > 0,
+              'friends': [lambda x: isinstance(x, str) and len(x.split()) == 2],
+              'events': [{'what': str,
+                          'when': float,
+                          'where': (int, int)}]}
+    data = {'name': 'henry',
+            'age': 99,
+            'friends': ['dave g', 'tom p'],
+            'events': [{'what': 'party',
+                        'when': 123.11,
+                        'where': (65, 73)},
+                       {'what': 'shopping',
+                        'when': 145.22,
+                        'where': [77, 44]}]}
+    assert s.schema.validate(schema, data) == data
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, s.dicts.merge(data, {'name': 123}))
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, s.dicts.merge(data, {'events': [None]}))
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, s.dicts.merge(data, {'events': [None] + data['events']}))
+    with pytest.raises(ValueError):
+        s.schema.validate(schema, s.dicts.merge(data, {'events': [{'what': 'shopping',
+                                                                   'when': 123.11,
+                                                                   'where': [0]}]}))
