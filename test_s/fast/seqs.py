7be971932cb22627b7f9a2633cff4d0ee2bafbec
nathants <me@nathants.com>
2014-11-23 21:48:40 -0800

    tests for s.seqs

--- test_s/fast/seqs.py
+++ test_s/fast/seqs.py
@@ -11,6 +11,11 @@ def test_dict_walk():
     assert s.seqs.walk({1: {2: 3}}, fn) == {2: {3: 4}}
 
 
+def test_dict_walk_transform():
+    fn = lambda x: isinstance(x, tuple) and len(x) == 2 and ['{}!!'.format(x[0]), x[1]] or x
+    assert s.seqs.walk({1: {2: 3}}, fn) == {'1!!': {'2!!': 3}}
+
+
 def test_dict_replace_kv_walk():
     fn = lambda x: x == (2, 3) and ('!', '?') or x
     assert s.seqs.walk({1: {2: 3}}, fn) == {1: {'!': '?'}}
@@ -26,3 +31,7 @@ def test_list_flatten():
 
 def test_dict_flatten():
     assert set(s.seqs.flatten({1: {2: 3}})) == {1, 2, 3}
+
+
+def test_value():
+    assert s.seqs.flatten(1) == (1,)
