cfb82ec091b17346f55e0f94f01346a1cbd3baf2
nathants <me@nathants.com>
2014-12-02 21:55:57 -0800

    tests

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -o pipefail
-ag -g "test_.*/fast/.*\.py$"|xargs py.test -xsvv --tb native
-ag -g "test_.*/fast/.*\.py$"|xargs py.test3 -xsvv --tb native
 ag -g ".*\.py$"|grep -v -e test_ -e __init__ -e setup.py|xargs python -m doctest
 ag -g ".*\.py$"|grep -v -e test_ -e __init__ -e setup.py|xargs python3 -m doctest
+ag -g "test_.*/fast/.*\.py$"|xargs py.test -xsvv --tb native
+ag -g "test_.*/fast/.*\.py$"|xargs py.test3 -xsvv --tb native
+ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test -xsvv --tb native
+ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test3 -xsvv --tb native
