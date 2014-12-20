3684c74825f8b08ec5551db8ce9ac1eb5a8ebac8
nathants <me@nathants.com>
2014-12-20 13:05:33 -0800

    dont stream logs in test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 set -o pipefail
 ag -g ".*\.py$"|grep -v -e test_ -e __init__ -e setup.py|xargs python -m doctest
 ag -g ".*\.py$"|grep -v -e test_ -e __init__ -e setup.py|xargs python3 -m doctest
-ag -g "test_.*/fast/.*\.py$"|xargs py.test -xsvv --tb native
-ag -g "test_.*/fast/.*\.py$"|xargs py.test3 -xsvv --tb native
-ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test -xsvv --tb native
-ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test3 -xsvv --tb native
+ag -g "test_.*/fast/.*\.py$"|xargs py.test -xvv --tb native
+ag -g "test_.*/fast/.*\.py$"|xargs py.test3 -xvv --tb native
+ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test -xvv --tb native
+ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test3 -xvv --tb native
