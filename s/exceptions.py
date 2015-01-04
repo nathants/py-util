6d2b90428897c5d3e78b7237de2fc03d79ea7a9a
nathants <me@nathants.com>
2015-01-03 21:27:18 -0800

    exceptions.update takes an optional predicate

--- s/exceptions.py
+++ s/exceptions.py
@@ -15,17 +15,18 @@ def ignore(*exceptions):
 
 
 @contextlib.contextmanager
-def update(fn_or_str, *exceptions):
+def update(fn_or_str, *exceptions, **kw):
     try:
         yield
     except Exception as e:
-        if type(e) in exceptions:
+        if type(e) in exceptions or not exceptions:
             try:
                 msg = e.args[0]
             except:
                 msg = ''
-            if isinstance(fn_or_str, types.FunctionType):
-                e.args = (fn_or_str(msg),) + e.args[1:]
-            else:
-                e.args = (msg + fn_or_str,) + e.args[1:]
+            if kw.get('when', lambda x: True)(msg):
+                if isinstance(fn_or_str, types.FunctionType):
+                    e.args = (fn_or_str(msg),) + e.args[1:]
+                else:
+                    e.args = (msg + fn_or_str,) + e.args[1:]
         raise
