026d0c523bb91ff5ee16294185815589fcf330d7
nathants <me@nathants.com>
2015-02-11 23:47:05 -0800

    tox depricates test.sh

--- test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -o pipefail
-ag -g ".*\.py$"|grep -v -e test_ -e __init__ -e setup.py|xargs python -m doctest
-ag -g ".*\.py$"|grep -v -e test_ -e __init__ -e setup.py|xargs python3 -m doctest
-ag -g "test_.*/fast/.*\.py$"|xargs py.test -xvv --tb native
-ag -g "test_.*/fast/.*\.py$"|xargs py.test3 -xvv --tb native
-ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test -xvv --tb native
-ag -g "test_.*/slow/.*\.py$"|xargs -n 1 -P 0 py.test3 -xvv --tb native
