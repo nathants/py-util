893b0042689bc2773e88651aeb49abbc7cbe798f
nathants <me@nathants.com>
2015-02-21 22:45:20 -0800

    service accepts a default

--- s/conf.py
+++ s/conf.py
@@ -1,7 +1,10 @@
 from __future__ import print_function, absolute_import
 import yaml
+import s
 
 
-def service(name, services_yml='/state/services.yml'):
-    with open(services_yml) as f:
-        return yaml.safe_load(f)[name]
+def service(name, services_yml='/state/services.yml', default=None):
+    with s.exceptions.ignore():
+        with open(services_yml) as f:
+            return yaml.safe_load(f)[name]
+    return default
