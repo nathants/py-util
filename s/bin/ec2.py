4fd83b2c7fea22aed906158e69a392901b4a95fa
nathants <me@nathants.com>
2015-02-28 15:23:26 -0800

    use cached funcs

--- s/bin/ec2.py
+++ s/bin/ec2.py
@@ -1,30 +1,33 @@
 import s
-import re
 import requests
 import os
 import yaml
 import boto.ec2
 
 
-with open(os.path.expanduser('~/.aws')) as f:
-    conf = yaml.safe_load(f)
+@s.cached.func
+def _conf():
+    with open(os.path.expanduser('~/.aws')) as f:
+        return yaml.safe_load(f)
 
 
-conn = boto.ec2.connect_to_region(**conf['connection'])
+@s.cached.func
+def _conn():
+    return boto.ec2.connect_to_region(**_conf()['connection'])
 
 
 def new():
-    val = conn.run_instances(**conf['ec2'])
+    val = _conn().run_instances(**_conf()['ec2'])
     print(val)
 
 
 def ls(state='running'):
-    for val in conn.get_only_instances(filters={'instance-state-name': state}):
+    for val in _conn().get_only_instances(filters={'instance-state-name': state}):
         print(val.id, '|', val.public_dns_name, '|', val.tags)
 
 
 def kill():
-    vals = conn.get_only_instances(filters={'instance-state-name': 'running'})
+    vals = _conn().get_only_instances(filters={'instance-state-name': 'running'})
     print('going to kill:')
     for val in vals:
         print('', val)
@@ -39,17 +42,17 @@ def auth(address=None):
     print('adding {address} to ssh security group.\nproceed? y/n'.format(**locals()))
     if input() == 'y':
         print('adding...')
-        conn.get_all_security_groups('ssh').pop().authorize('tcp', 22, 22, address)
+        _conn().get_all_security_groups('ssh').pop().authorize('tcp', 22, 22, address)
 
 
 def auths():
-    for rule in conn.get_all_security_groups('ssh').pop().rules:
+    for rule in _conn().get_all_security_groups('ssh').pop().rules:
         for grant in rule.grants:
             print(grant.cidr_ip)
 
 
 def revoke():
-    group = conn.get_all_security_groups('ssh').pop()
+    group = _conn().get_all_security_groups('ssh').pop()
     vals = [{'group_id': group.id,
              'ip_protocol': rule.ip_protocol,
              'from_port': rule.from_port,
@@ -63,7 +66,7 @@ def revoke():
     print('proceed? y/n')
     if input() == 'y':
         for val in vals:
-            conn.revoke_security_group(**val)
+            _conn().revoke_security_group(**val)
 
 
 def main():
