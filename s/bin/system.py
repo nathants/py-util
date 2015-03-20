60b505d29c309c974410138b136708f9bf18d355
nathants <me@nathants.com>
2015-03-19 22:25:52 -0700

    hacking on system

--- s/bin/system.py
+++ s/bin/system.py
@@ -1,4 +1,5 @@
 import yaml
+import re
 import itertools
 import sys
 import argh
@@ -9,6 +10,10 @@ import s.dicts
 import s.net
 import s.web
 import os
+
+
+class schemas:
+    data = {}
 
 
 _services = './docker/state/services.yml'
@@ -27,25 +32,45 @@ def run(action_name, cmd, append_to_cmd=None, tty=False, no_clean_before=False, 
         stop(action_name)
     try:
         _start_deps(action)
-        print('start main')
-        try:
-            s.shell.run(_run_cmd(action['main'], tty, cmd, append_to_cmd), interactive=True)
-        except:
-            sys.exit(1)
+        ports(action_name)
+        if 'main' in action:
+            print('start main')
+            try:
+                s.shell.run(_run_cmd(action['main'], tty, cmd, append_to_cmd), interactive=True)
+            except:
+                sys.exit(1)
     finally:
         if not no_clean_after:
             stop(action_name)
 
 
+def logs(action_name, container_name=None):
+    action = _get_action(action_name)
+    if container_name:
+        1/0 # FIXME
+        print(_exposed_ports(action[container_name]['tag']))
+    else:
+        procs = []
+        for container_name, data in action.items():
+            print(container_name)
+            procs.append(s.proc.new(s.shell.run, 'docker logs -f', container_name, stream=True))
+        for p in procs:
+            p.join()
+
+
 def ports(action_name, container_name=None):
     action = _get_action(action_name)
     if container_name:
-        print(_exposed_port(action[container_name]['tag']))
+        1/0 # FIXME
+        print(_exposed_ports(action[container_name]['tag']))
     else:
         for container_name, data in action.items():
-            port = _exposed_port(data['tag'])
-            if port:
-                print('{container_name}:{port}'.format(**locals()))
+            ports = _exposed_ports(data['tag'])
+            if ports:
+                print("")
+                print(container_name)
+                for port in sorted(ports, key=lambda x: x['internal']):
+                    print(' http://localhost:%(external)s -> docker:%(internal)s' % port)
 
 
 def stop(action_name):
@@ -91,10 +116,10 @@ def _main():
     s.shell.dispatch_commands(globals(), __name__)
 
 
-def _exposed_port(tag):
-    port = s.shell.run('sudo docker port', tag).split(':')[-1]
-    if port:
-        return int(port)
+def _exposed_ports(tag):
+    return [s.dicts.map(lambda k, v: [k, int(v)],
+                        re.search(r'(?P<internal>\d+).*-> 0.0.0.0:(?P<external>\d+)', x).groupdict())
+            for x in s.shell.run('sudo docker port', tag).splitlines()]
 
 
 def _start_deps(action):
@@ -103,31 +128,37 @@ def _start_deps(action):
     if to_start:
         started = []
         for i in itertools.count():
-            container_name, data = to_start.pop()
+            container_name, data = to_start.pop(0)
             deps = data.get('depends', [])
             if not deps or all(x in started for x in deps):
-                print('start dep:', container_name)
+                # TODO supervise this process
                 s.shell.run(_run_cmd(data, bg=True))
                 started.append(container_name)
-                data = {'port': _exposed_port(data['tag']),
-                        'host': _host_ip()}
-                with open(_services, 'a') as f:
-                    f.write(yaml.dump({container_name: data}))
-                s.web.wait_for_http('http://{host}:{port}'.format(**data))
+                ports = _exposed_ports(data['tag'])
+                if len(ports) == 1:
+                    data = {'port': ports[0],
+                            'host': _host_ip()}
+                    with open(_services, 'a') as f:
+                        f.write(yaml.dump({container_name: data}))
+                elif len(ports) > 1:
+                    data = {'ports': {port['internal']: port['external'] for port in ports},
+                            'host': _host_ip()}
+                    with open(_services, 'a') as f:
+                        f.write(yaml.dump({container_name: data}))
+                # s.web.wait_for_http('http://{host}:{port}'.format(**data))
             else:
                 to_start.append([container_name, data])
-            assert i < 500, 'never resolved dependency order. remaining: {}'.format(to_start)
+            assert i < 5, 'never resolved dependency order. remaining: {}'.format(to_start)
             if not to_start:
                 return
 
 
 def _tag_containers(action_name, action):
     for container_name, data in action.items():
-        action[container_name]['tag'] = '_'.join([
-            os.path.basename(os.getcwd()),
-            action_name,
-            container_name
-        ])
+        action[container_name]['tag'] = (action[container_name].get('image')
+                                         or '_'.join([os.path.basename(os.getcwd()),
+                                                      action_name,
+                                                      container_name]))
     return action
 
 
@@ -153,6 +184,18 @@ def _build_cmd(data, nocache, pull):
     ])
 
 
+def _read_env(name):
+    print('readenv', name)
+    if not isinstance(name, str) or not name.startswith('$services.'):
+        return name
+    with open(_services) as f:
+        keys = [int(x) if x.isdigit() else x
+                for x in name.split('$services.')[-1].split('.')]
+
+        print("wtf?", name, keys)
+        return s.dicts.get(yaml.safe_load(f), keys)
+
+
 def _run_cmd(data, tty=False, cmd=None, append_to_cmd=None, bg=False):
     append_to_cmd = append_to_cmd or ''
     return ' '.join(
@@ -161,11 +204,13 @@ def _run_cmd(data, tty=False, cmd=None, append_to_cmd=None, bg=False):
          '-it' if tty else '',
          '-d' if bg else '',
          '--name={tag}'.format(**data),
+         '--memory={memory}'.format(**data) if 'memory' in data else '',
          '-v {}/docker/state:/state'.format(os.getcwd())] +
         ['-v {}:{}'.format(os.path.abspath(os.path.expanduser(a)), b)
          for x in data.get('volumes', [])
          for y in [str(x).split(':')]
          for a, b in [(y + y)[:2]]] +
+        ['--env {}={}'.format(k, _read_env(v)) for k, v in data.get('env', {}).items()] +
         ['-p {}:{}'.format(*z)
          for x in data.get('ports', [])
          for y in [str(x).split(':')]
