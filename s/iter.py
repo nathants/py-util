a757e08bdadc6b8aef4874b6e947c551fc69ea21
nathants <me@nathants.com>
2015-04-02 21:46:57 -0700

    chunk and nwise

--- s/iter.py
+++ s/iter.py
@@ -1,6 +1,25 @@
 import itertools
+import six
 
 
-def groupby(val, key):
+def groupby(val, key, realize=True):
     val = sorted(val, key=key)
-    return [(k, list(v)) for k, v in itertools.groupby(val, key=key)]
+    if realize:
+        return [(k, list(v)) for k, v in itertools.groupby(val, key=key)]
+    else:
+        return itertools.groupby(val, key=key)
+
+
+def nwise(val, n):
+    return six.moves.zip(*(itertools.islice(val, i, None) for i, val in enumerate(itertools.tee(val, n))))
+
+
+def chunk(val, n, drop_extra=False):
+    res = ()
+    for x in val:
+        res += (x,)
+        if len(res) == n:
+            yield res
+            res = ()
+    if res and not drop_extra:
+        yield res
