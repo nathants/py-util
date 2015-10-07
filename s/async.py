5ca24c6bad0c251384ae9d469e49cf002cc0da54
nathants <me@nathants.com>
2015-10-06 21:13:33 -0700

    exceptions kill

--- s/async.py
+++ s/async.py
@@ -1,8 +1,30 @@
 from __future__ import print_function, absolute_import
 import s.exceptions
+import os
+import signal
+import functools
+import logging
 
 
 def is_future(obj):
     with s.exceptions.ignore(AttributeError):
         object.__getattribute__(obj, 'add_done_callback')
         return True
+
+
+def exceptions_kill_pid(decoratee):
+    """
+    Any exceptions in this decoratee will exit the main process,
+    even when this decoratee is running in a thread.
+    """
+    exit1 = lambda: os.kill(os.getpid(), signal.SIGTERM)
+    @functools.wraps(decoratee)
+    def f(*a, **kw):
+        try:
+            return decoratee(*a, **kw)
+        except SystemExit:
+            exit1()
+        except:
+            logging.exception('')
+            exit1()
+    return f
