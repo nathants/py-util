4d1fe20ecfc0b6982933a8c9b622b1b86da2be5e
nathants <me@nathants.com>
2018-03-04 20:04:18 -0800

    requires py36+

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuptools.setup(
     license='mit',
     name='py-util',
     author='nathan todd-stone',
+    python_requires='>=3.6',
     author_email='me@nathants.com',
     url='http://github.com/nathants/py-util',
     packages=['util'],
