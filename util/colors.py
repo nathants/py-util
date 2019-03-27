436b72816729a8d73abbe35ba02f5fc2bbae60fa
nathants <me@nathants.com>
2019-03-27 11:26:56 -0700

    force color

--- util/colors.py
+++ util/colors.py
@@ -1,19 +1,17 @@
 import functools
 import sys
 
+force = False
 
 _colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
-
 _pairs = zip(_colors, range(31, 38))
 
-
 def _make_color(code, text):
-    if sys.stdout.isatty():
+    if force or sys.stdout.isatty():
         return "\033[{}m{}\033[0m".format(code, text)
     else:
         return text
-
 
 for _color, _num in _pairs:
     locals()[_color] = functools.partial(_make_color, _num)
