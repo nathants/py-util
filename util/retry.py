a73cbadab5e4803971c8572e14547230bae13bab
nathants <me@nathants.com>
2020-03-15 19:02:47 -0700

    empty exception should format a stack trace for retry logging

--- util/retry.py
+++ util/retry.py
@@ -1,6 +1,7 @@
 import itertools
 import logging
 import time
+import traceback
 
 def retry(f, *allowed_exceptions, allowed_exception_fn=None, times=6, sleep=1, exponent=1, silent=False, max_seconds=0):
     def fn(*a, **kw):
@@ -17,6 +18,7 @@ def retry(f, *allowed_exceptions, allowed_exception_fn=None, times=6, sleep=1, e
                 if i == times or (max_seconds and duration >= max_seconds):
                     raise
                 if not silent:
+                    e = e if str(e).strip() else '\n' + traceback.format_exc()
                     logging.info(f'retrying: {f.__module__}.{f.__name__}, because of: {e}')
                 if exponent:
                     amount = (sleep * i) ** exponent
