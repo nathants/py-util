97cccd4b6ec21e384d6dbb68123db4aa995a1b75
nathants <me@nathants.com>
2020-06-18 17:21:43 -0700

    retry should default to showing stacktrace

--- util/retry.py
+++ util/retry.py
@@ -3,7 +3,7 @@ import logging
 import time
 import traceback
 
-def retry(f, *allowed_exceptions, allowed_exception_fn=None, times=6, sleep=1, exponent=1, silent=False, max_seconds=0):
+def retry(f, *allowed_exceptions, allowed_exception_fn=None, times=6, sleep=1, exponent=1, silent=False, max_seconds=0, stacktrace=True):
     def fn(*a, **kw):
         start = time.time()
         for i in itertools.count():
@@ -18,8 +18,11 @@ def retry(f, *allowed_exceptions, allowed_exception_fn=None, times=6, sleep=1, e
                 if i == times or (max_seconds and duration >= max_seconds):
                     raise
                 if not silent:
-                    e = e if str(e).strip() else '\n' + traceback.format_exc()
-                    logging.info(f'retrying: {f.__module__}.{f.__name__}, because of: {e}')
+                    if stacktrace:
+                        logging.exception(f'retrying: {f.__module__}.{f.__name__}')
+                    else:
+                        e = e if str(e).strip() else '\n' + traceback.format_exc()
+                        logging.info(f'retrying: {f.__module__}.{f.__name__}, because of: {e}')
                 if exponent:
                     amount = (sleep * i) ** exponent
                 else:
