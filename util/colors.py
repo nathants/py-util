3593a22dc56da2e7a71068ca7b3e530dd96d85d2
nathants <me@nathants.com>
2020-12-03 23:08:52 -0800

    make colors more explicit

--- util/colors.py
+++ util/colors.py
@@ -1,12 +1,7 @@
-import functools
 import sys
 import os
 
 force = 'COLORS' in os.environ
-
-_colors = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
-
-_pairs = zip(_colors, range(31, 38))
 
 def _make_color(code, text):
     if force or sys.stdout.isatty():
@@ -14,5 +9,10 @@ def _make_color(code, text):
     else:
         return text
 
-for _color, _num in _pairs:
-    locals()[_color] = functools.partial(_make_color, _num)
+red     = lambda text: _make_color(31, text)
+green   = lambda text: _make_color(32, text)
+yellow  = lambda text: _make_color(33, text)
+blue    = lambda text: _make_color(34, text)
+magenta = lambda text: _make_color(35, text)
+cyan    = lambda text: _make_color(36, text)
+white   = lambda text: _make_color(37, text)
